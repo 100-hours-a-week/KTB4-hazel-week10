65ae7d1fc8a7545f19de3e495c6c1e5575041888
hyejun <hyejunkkim228@gmail.com>
2026-08-06 10:29:23 +0900

    refactor: router lazy처리

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,41 +1,44 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import LoginPage from "@/pages/loginPage/index.jsx";
-import SignupPage from "@/pages/signupPage/index.jsx";
+const LoginPage = lazy(() => import("@/pages/loginPage/index.jsx"));
+const SignupPage = lazy(() => import("@/pages/signupPage/index.jsx"));
 
-import BoardPage from "@/pages/boardPage/index.jsx";
-import BoardWritePage from "@/pages/postWritePage/index.jsx";
-import BoardDetailPage from "@/pages/boardDetailPage/index.jsx";
-import BoardEditPage from "@/pages/boardEditPage/index.jsx";
-import SelectedBoardPage from "@/pages/selectedBoardPage/index.jsx";
+const BoardPage = lazy(() => import("@/pages/boardPage/index.jsx"));
+const BoardWritePage = lazy(() => import("@/pages/postWritePage/index.jsx"));
+const BoardDetailPage = lazy(() => import("@/pages/boardDetailPage/index.jsx"));
+const BoardEditPage = lazy(() => import("@/pages/boardEditPage/index.jsx"));
+const SelectedBoardPage = lazy(() => import("@/pages/selectedBoardPage/index.jsx"));
 
-import UserEditPage from "@/pages/userEditPage/index.jsx";
-import PasswordEditPage from "@/pages/passwordEditPage/index.jsx";
-import NotificationSettingsPage from "@/pages/notificationSettingsPage/index.jsx";
-import DiscordCallbackPage from "@/pages/discordCallbackPage/index.jsx";
+const UserEditPage = lazy(() => import("@/pages/userEditPage/index.jsx"));
+const PasswordEditPage = lazy(() => import("@/pages/passwordEditPage/index.jsx"));
+const NotificationSettingsPage = lazy(() => import("@/pages/notificationSettingsPage/index.jsx"));
+const DiscordCallbackPage = lazy(() => import("@/pages/discordCallbackPage/index.jsx"));
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
 
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
 
-        <Route path="/boards" element={<BoardPage />} />
-        <Route path="/boards/write" element={<BoardWritePage />} />
-        <Route path="/boards/selected" element={<SelectedBoardPage />} />
-        <Route path="/boards/:postId" element={<BoardDetailPage />} />
-        <Route path="/boards/:postId/edit" element={<BoardEditPage />} />
+          <Route path="/boards" element={<BoardPage />} />
+          <Route path="/boards/write" element={<BoardWritePage />} />
+          <Route path="/boards/selected" element={<SelectedBoardPage />} />
+          <Route path="/boards/:postId" element={<BoardDetailPage />} />
+          <Route path="/boards/:postId/edit" element={<BoardEditPage />} />
 
-        <Route path="/users/edit" element={<UserEditPage />} />
-        <Route path="/users/password" element={<PasswordEditPage />} />
-        <Route path="/users/notifications" element={<NotificationSettingsPage />} />
-        <Route path="/auth/discord/callback" element={<DiscordCallbackPage />} />
+          <Route path="/users/edit" element={<UserEditPage />} />
+          <Route path="/users/password" element={<PasswordEditPage />} />
+          <Route path="/users/notifications" element={<NotificationSettingsPage />} />
+          <Route path="/auth/discord/callback" element={<DiscordCallbackPage />} />
 
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
